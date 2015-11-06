53b95abf1882a5dbcad4bdd85f64fdbdf10bace2
Zhang Hao <zhangghaoo@gmail.com>
2015-11-06 23:16:40 +0800

    Dijkstra's algorithm added Move algorithm code to package "brain"

---
+++ app/src/androidTest/java/brain/dijkstra/OnCallDijkstra.java
@@ -0,0 +1,7 @@
+package brain.dijkstra;
+
+/**
+ * Created by zhanghao on 6/11/15.
+ */
+public class OnCallDijkstra {
+}
--- app/src/main/java/com/sutd/zhangzhexian/travelapp/MapView.java
+++ app/src/main/java/com/sutd/zhangzhexian/travelapp/MapView.java
@@ -1,15 +1,11 @@
 package com.sutd.zhangzhexian.travelapp;
 
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,12 +14,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Hashtable;
-import java.util.List;
 
 /**
  * Created by Lakshita on 11/4/2015.
@@ -37,7 +27,6 @@ public class MapView extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.mapview, container, false);
-        // TODO: 5/11/15 Some Problem with Fragment
         //only for activity
         /*setContentView(R.layout.activity_maps);*/
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -52,13 +41,12 @@ public class MapView extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(34, 117);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Beijing"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng singapore = new LatLng(1.3, 103.8
 
-        LatLng beijing = new LatLng(67,88);
-        mMap.addMarker(new MarkerOptions().position(beijing).title("Somewhere"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(beijing));
+        );
+        mMap.addMarker(new MarkerOptions().position(singapore).title("Singapore"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(singapore));
+
 
         mMap.addPolyline(new PolylineOptions());
     }
