30442d159fc0c33898e940e11a30d1a1f8e5aea8
ndmnh <ndminh208@gmail.com>
2015-11-07 23:54:32 +0800

    Updating attraction locator

--- app/src/main/java/com/sutd/zhangzhexian/travelapp/AttractionLocator.java
+++ app/src/main/java/com/sutd/zhangzhexian/travelapp/AttractionLocator.java
@@ -35,8 +35,7 @@ import java.util.Locale;
 /**
  * Created by Lakshita on 11/4/2015.
  */
-public class AttractionLocator extends Fragment implements OnMapReadyCallback
-/*  ,ConnectionCallbacks, OnConnectionFailedListener*/ {
+public class AttractionLocator extends Fragment implements OnMapReadyCallback {
 
     GoogleMap mMap;
     View root;
@@ -44,9 +43,6 @@ public class AttractionLocator extends Fragment implements OnMapReadyCallback
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.attraction_locator, container, false);
-        // TODO: 5/11/15 Some Problem with Fragment
-        //only for activity
-        /*setContentView(R.layout.activity_maps);*/
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
@@ -132,12 +128,94 @@ public class AttractionLocator extends Fragment implements OnMapReadyCallback
     // Robust search, returns corrected name location
     public static String correctedSearch(String search){
         ArrayList<String> list = new ArrayList<>();
-        list.add("Sentosa");
-        list.add("Marina Bay Sands");
-        list.add("Changi Airport");
-        list.add("Orchard Road");
+        list.add("Abdul Gaffoor Mosque");
+        list.add("Al-Abrar Mosque");
+        list.add("ArtScience Museum");
+        list.add("Asian Civilisations Museum");
+        list.add("Bright Hill Temple (Khong Meng San Phor Kark See Temple)");
+        list.add("Buddha Tooth Relic Temple");
+        list.add("Bukit Timah Nature Reserve");
+        list.add("Cathedral of the Good Shepherd");
+        list.add("Central Catchment Nature Reserve");
+        list.add("Central Sikh Temple");
+        list.add("Changi Prison Chapel and Museum");
+        list.add("Chijmes");
+        list.add("Chinatown Heritage Centre");
+        list.add("Chinese and Japanese Gardens");
+        list.add("Chinese Methodist Church");
+        list.add("Church of St Gregory the Illuminator");
+        list.add("Crane Dance");
+        list.add("East Coast Park");
+        list.add("Esplanade -Theatres on the Bay");
+        list.add("Eu Yan Sang Chinese Medical Hall");
+        list.add("Fort Canning Park");
+        list.add("Gardens by the Bay");
+        list.add("Geylang Serai Market");
+        list.add("G-Max Reverse Bungy");
+        list.add("Hajjah Fatimah Mosque");
+        list.add("Haw Par Villa");
+        list.add("HortPark");
+        list.add("House of Tan Teng Niah");
+        list.add("Images of Singapore");
+        list.add("Istana");
+        list.add("Jamae Mosque");
+        list.add("Joo Chiat Street");
+        list.add("Jurong Bird Park");
+        list.add("Katong");
+        list.add("Kranji War Memorial");
+        list.add("Kusu Island");
+        list.add("Kwan Im Thong Hood Cho Temple");
+        list.add("Lau Pa Sat (Telok Ayer Market)");
+        list.add("Leong San See Temple");
+        list.add("Maghain Aboth Synagogue");
+        list.add("Malay Heritage Centre");
+        list.add("Malay Village");
+        list.add("Mandai Orchid Gardens");
+        list.add("Marina Barrage");
+        list.add("Marina Bay Sands Casino");
+        list.add("Marina Bay Sands SkyPark");
+        list.add("Maxwell Road Hawker Centre");
+        list.add("Merlion Park");
+        list.add("Mount Faber Park");
+        list.add("Nagore Durgha Shrine");
+        list.add("National Museum of Singapore");
+        list.add("National Orchid Garden");
+        list.add("NEWater Visitor Centre");
+        list.add("Old Parliament House");
+        list.add("Peranakan Museum");
         list.add("Pulau Ubin");
-
+        list.add("Raffles Hotel");
+        list.add("Raffles Place");
+        list.add("Red Dot Design Museum");
+        list.add("Resort World Sentosa Casino");
+        list.add("Sentosa Skyline Luge and Skyride");
+        list.add("Singapore Art Museum");
+        list.add("Singapore Bazaar and Flea Market");
+        list.add("Singapore Botanic Gardens");
+        list.add("Singapore Butterfly and Insect Kingdom");
+        list.add("Singapore Discovery Centre");
+        list.add("Singapore F1 Grand Prix");
+        list.add("Singapore Flyer");
+        list.add("Singapore Mint Coin Gallery");
+        list.add("Singapore Navy Museum");
+        list.add("Singapore Night Safari");
+        list.add("Singapore Philatelic Museum");
+        list.add("Singapore River");
+        list.add("Science Centre Singapore");
+        list.add("Singapore Zoo");
+        list.add("Siong Lim Temple");
+        list.add("Sisters' Islands");
+        list.add("Songs of the Sea");
+        list.add("Sri Krishnan Temple");
+        list.add("Sri Mariamman Temple");
+        list.add("Sri Srinivasa Perumal Temple");
+        list.add("Sri Thandayuthapani Temple");
+        list.add("Sri Veeramakaliamman Temple");
+        list.add("St Andrew's Cathedral");
+        list.add("Statues of Sir Stamford Raffles");
+        list.add("St John's Island");
+        list.add("Sultan Mosque");
+        list.add("Sungei Buloh Nature Park");
 
         ArrayList<Integer> myList = new ArrayList<>();
         Hashtable balance = new Hashtable();
@@ -149,62 +227,4 @@ public class AttractionLocator extends Fragment implements OnMapReadyCallback
         Collections.sort(myList);
         return (String) balance.get(myList.get(0));
     }
-
-    // Here is where I get the current location of the user (aka Last Known Location of device)
-
-    /*protected static final String TAG = "AttractionLocator";
-    *//**
-     * Provides the entry point to Google Play services.
-     *//*
-    protected GoogleApiClient mGoogleApiClient;
-
-    *//**
-     * Represents a geographical location.
-     *//*
-
-    protected Location mLastLocation;
-    protected double currentLat;
-    protected double currentLong;
-
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-    @Override
-    public void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        // Provides a simple way of getting a device's location and is well suited for
-        // applications that do not require a fine-grained location and that do not need location
-        // updates. Gets the best and most recent location currently available, which may be null
-        // in rare cases when a location is not available.
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        currentLat = mLastLocation.getLatitude();
-        currentLong = mLastLocation.getLongitude();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-    }
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection to Google Play services was lost for some reason. We call connect() to
-        // attempt to re-establish the connection.
-        Log.i(TAG, "Connection suspended");
-        mGoogleApiClient.connect();
-    }*/
 }
