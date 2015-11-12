7963cc92b9e21a4371270f4d22feed09b948c72f
ndmnh <ndminh208@gmail.com>
2015-11-12 09:44:52 +0800

    Update AttractionList

--- app/src/main/java/com/sutd/zhangzhexian/travelapp/AttractionList.java
+++ app/src/main/java/com/sutd/zhangzhexian/travelapp/AttractionList.java
@@ -91,7 +91,7 @@ public class AttractionList extends Fragment implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.add:      //add button clicked
                 String searchText = Attraction.getText().toString();
-                String locationName = correctedSearch(searchText);
+                String locationName = AttractionLocator.correctedSearch(searchText);
                 attractList.add(locationName);      //add attraction
                 adapter.add(locationName);
                 Attraction.setText(""); //this sets textbox to null
@@ -107,37 +107,4 @@ public class AttractionList extends Fragment implements View.OnClickListener {
                         .commit();
         }
     }
-
-    public static int minDistance(String a, String b) {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        // i == 0
-        int [] costs = new int [b.length() + 1];
-        for (int j = 0; j < costs.length; j++)
-            costs[j] = j;
-        for (int i = 1; i <= a.length(); i++) {
-            // j == 0; nw = lev(i - 1, j)
-            costs[0] = i;
-            int nw = i - 1;
-            for (int j = 1; j <= b.length(); j++) {
-                int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
-                nw = costs[j];
-                costs[j] = cj;
-            }
-        }
-        return costs[b.length()];
-    }
-
-    public static String correctedSearch(String search){
-        ArrayList<Integer> myList = new ArrayList<>();
-        Hashtable balance = new Hashtable();
-        for (int i=1; i < DataBase.attractionNames.length; i++){
-            int value = minDistance(search, DataBase.attractionNames[i]);
-            balance.put(value,DataBase.attractionNames[i]);
-            myList.add(value);
-        }
-        Collections.sort(myList);
-        return (String) balance.get(myList.get(0));
-    }
-
 }
