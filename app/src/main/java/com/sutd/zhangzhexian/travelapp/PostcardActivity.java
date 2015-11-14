19b33f4a2ccc58e2f7eac99382da7eba9d488169
chngws <weisheng_chng@mymail.sutd.edu.sg>
2015-11-14 14:17:40 +0800

    Postcard function

--- app/src/main/java/com/sutd/zhangzhexian/travelapp/PostcardActivity.java
+++ app/src/main/java/com/sutd/zhangzhexian/travelapp/PostcardActivity.java
@@ -1,11 +1,20 @@
 package com.sutd.zhangzhexian.travelapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by Lakshita on 11/4/2015.
@@ -13,13 +22,44 @@ import android.view.ViewGroup;
 public class PostcardActivity extends Fragment {
 
     View root;
+
+    private static int LOAD_IMAGE_RESULTS = 1;
+    private Button button;
+    private ImageView image;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.picture_postcard, container, false);
+        super.onCreate(savedInstanceState);
 
+        button = (Button) root.findViewById(R.id.button);
+        image = (ImageView) root.findViewById(R.id.image);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, LOAD_IMAGE_RESULTS);
+            }
+        });
 
         return root;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOAD_IMAGE_RESULTS && resultCode == Activity.RESULT_OK && data != null) {
+            Uri pickedImage = data.getData();
+            String[] filePath = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContext().getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+            image.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+            Toast.makeText(getActivity().getApplicationContext(), "Take a screenshot to save! ", Toast.LENGTH_LONG).show();
+            cursor.close();
+        }
+    }
 }
+
+
