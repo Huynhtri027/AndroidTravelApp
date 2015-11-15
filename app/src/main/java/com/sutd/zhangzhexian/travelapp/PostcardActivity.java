c5e0c808d1a6e702dfe079eacb2b3da3fa402251
chngws <weisheng_chng@mymail.sutd.edu.sg>
2015-11-15 21:40:28 +0800

    Postcard function

--- app/src/main/java/com/sutd/zhangzhexian/travelapp/PostcardActivity.java
+++ app/src/main/java/com/sutd/zhangzhexian/travelapp/PostcardActivity.java
@@ -19,44 +19,54 @@ import android.widget.Toast;
 /**
  * Created by Lakshita on 11/4/2015.
  */
-public class PostcardActivity extends Fragment {
+public class PostcardActivity extends Fragment implements View.OnClickListener {
 
     View root;
 
-    private static int LOAD_IMAGE_RESULTS = 1;
-    private Button button;
-    private ImageView image;
+    public Button button1;
+    public Button button2;
+    public ImageView image;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.picture_postcard, container, false);
-        super.onCreate(savedInstanceState);
-
-        button = (Button) root.findViewById(R.id.button);
-        image = (ImageView) root.findViewById(R.id.image);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, LOAD_IMAGE_RESULTS);
-            }
-        });
-
+        button1 = (Button) root.findViewById(R.id.button1);
+        button2 = (Button) root.findViewById(R.id.button2);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Intent i1 = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i1, 1);
+                break;
+
+            case R.id.button2:
+                Toast.makeText(getActivity().getApplicationContext(), "Take a screenshot to save!", Toast.LENGTH_LONG).show();
+                break;
+            }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == LOAD_IMAGE_RESULTS && resultCode == Activity.RESULT_OK && data != null) {
+        if (requestCode == 1964 && resultCode == Activity.RESULT_OK && data != null) {
             Uri pickedImage = data.getData();
             String[] filePath = {MediaStore.Images.Media.DATA};
             Cursor cursor = getContext().getContentResolver().query(pickedImage, filePath, null, null, null);
-            cursor.moveToFirst();
+            try{
+                cursor.moveToFirst();}
+            catch(NullPointerException e){
+                Toast.makeText(getActivity().getApplicationContext(), "NullPointerException", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
             String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
             image.setImageBitmap(BitmapFactory.decodeFile(imagePath));
-            Toast.makeText(getActivity().getApplicationContext(), "Take a screenshot to save! ", Toast.LENGTH_LONG).show();
             cursor.close();
         }
     }
